777b3fe0ce8343368c35589c7e422a17cfb1a07b
seungmoo2901 <100752628+seungmoo2901@users.noreply.github.com>
2024-10-17 15:12:44 +0900

    Add files via upload

---
+++ 241017/semiProject01/src/main/java/com/semiproject/hotels/HomeController.java
@@ -0,0 +1,37 @@
+package com.semiproject.hotels;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		return "home";
+	}
+	
+	@RequestMapping(value = "/login/", method = RequestMethod.GET)
+	public String login(Locale locale, Model model) {
+		return "login/login";
+	}
+	
+	@RequestMapping(value = "/emp/", method = RequestMethod.GET)
+	public String emp(Locale locale, Model model) {
+		return "emp/add";
+	}
+	@RequestMapping(value = "/intro/", method = RequestMethod.GET)
+	public String intro(Locale locale, Model model) {
+		return "intro/travle";
+	}
+}
